4e397ba735cb28196053132fbbe6f613b6a2ad5e
Asman Bhusal <abhusal1@outlook.com>
2023-02-21 16:50:42 +0000

    creating a function

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// function that takes string argument and also returns string value
+func sayHello(name string) string {
+	return "Hello, " + name
+}
+
 func main() {
-	fmt.Println("Hello World")
+	// calling the sayHello function along with passing the string value
+	fmt.Println(sayHello("Asman"))
 }

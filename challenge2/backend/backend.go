398ade8df78ff01a337dc16ba9f77c2cec313489
Asman Bhusal <abhusal1@outlook.com>
2023-02-22 13:53:58 +0000

    challenge2

---
+++ challenge2/backend/backend.go
@@ -0,0 +1,32 @@
+package backend
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type App struct {
+	DB   *sql.DB
+	Port string
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world \n")
+}
+
+func (a *App) Initialize() {
+	a.DB, err := sql.Open("sqlite3", "../../practiceit.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func (a *App) Run() {
+	http.HandleFunc("/", helloWorld)
+	fmt.Println("Server started and listening on host:", a.Port)
+	log.Fatal(http.ListenAndServe(a.Port, nil))
+}

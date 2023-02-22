29ecef4297156632c67111b572e06b6d326dd001
Asman Bhusal <abhusal1@outlook.com>
2023-02-22 12:05:06 +0000

    Methods

--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import "fmt"
 
-func dimensions(length, width, height int) (area int, volume int) {
-	area = length * width
-	volume = length * width * height
-	return
+type Dimension struct {
+	length int
+	width  int
+	height int
+}
+
+// this is method in golang
+// methods are the functions with recevier arguments
+
+/*
+(d Dimension) is the receiver type
+Area() is the name of the method which does not take any arguments
+and Area() return one int type
+*/
+func (d Dimension) Area() int {
+	return d.length * d.width
+}
+
+func (d Dimension) Volume() int {
+	return d.height * d.length * d.width
 }
 
 func main() {
-	area, volume := dimensions(5, 5, 5)
-	fmt.Println("area: ", area, ", volume:", volume)
+	d := Dimension{10, 5, 6}
+	fmt.Println("area: ", d.Area())
+	fmt.Println("volume: ", d.Volume())
 }
